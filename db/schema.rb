9145b4ff4b93b678b23a8249ea44fe211f914c42
Brian Iversen <biversen21@gmail.com>
2014-07-07 11:53:49 -0700

    finished modeling users

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20140707182420) do
 
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
