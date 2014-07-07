356e0393909f968e327dac134bda72db81a70f7e
Brian Iversen <biversen21@gmail.com>
2014-07-06 19:31:53 -0700

    Added specs for titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
   end
   
   describe "Help page" do
@@ -15,6 +20,24 @@ describe "StaticPages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Help')
+    end
+  end
+  
+  describe "About page" do
+    
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
     end
   end
 end
