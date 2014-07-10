1e87e46d882ea1e8fda9528817a52274d2f3ea5a
Brian Iversen <biversen21@gmail.com>
2014-07-10 12:51:07 -0700

    added follow/unfollow buttons and specs

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,12 @@ Rails.application.routes.draw do
 
   resources :microposts, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
